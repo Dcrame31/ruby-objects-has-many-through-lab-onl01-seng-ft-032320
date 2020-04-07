67d1b7f2eff1f5b99982d6413279d93d2cd0d088
Diane Crame <dcrame@icloud.com>
2020-04-07 20:39:49 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -17,8 +17,8 @@ class Doctor
   end
   
   def appointments
-    Appointment.all.select do |appt|
-      appt.doctor == self
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
     end
   end
   
